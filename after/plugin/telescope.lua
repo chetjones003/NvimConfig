772635521ebc50c8d36578f2cbdf3d2b0bc285f1
Chet <chetjones003@gmail.com>
2023-02-05 10:45:47 -0600

    feat: add barbar plugin, buffer remaps, and lualine changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>bf', builtin.buffers,{})
