c81ce6b85e67dc2747ba1022eadef3cb271ad602
Chet <chetjones003@gmail.com>
2023-02-05 10:34:56 -0600

    feat: add barbar plugin

--- lua/cj/packer.lua
+++ lua/cj/packer.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use('romgrk/barbar.nvim')
     use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
